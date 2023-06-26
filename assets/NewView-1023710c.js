ef3040227113c448de6f2327940bb690115227d6
Viknesh Krishnan <vmkrish@umich.edu>
2023-06-27 01:47:51 +0900

    Updates

---
+++ assets/NewView-1023710c.js
@@ -0,0 +1,1 @@
+import{d as e,o as n,j as o}from"./index-3339df3f.js";const s=e({__name:"NewView",setup(t){return(a,r)=>(n(),o("h1",null," The new view! "))}});export{s as default};
--- assets/NewView-1817f35d.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as n,i as o}from"./index-2d4ff739.js";const s=e({__name:"NewView",setup(t){return(a,r)=>(n(),o("h1",null," The new view! "))}});export{s as default};
